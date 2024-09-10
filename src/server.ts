ecb38aa2f2fcbcd48afcc7bfb7b715ceab3db44f
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-09-09 23:35:28 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,52 @@
+import fastify from "fastify";
+import { ZodTypeProvider, jsonSchemaTransform, serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { auth } from './middleware/verify-jwt';
+
+import multer from "fastify-multer";
+
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+
+import fastifyCookie from '@fastify/cookie';
+import fastifyCors from "@fastify/cors";
+import fastifyJwt from "@fastify/jwt";
+
+const {register, listen, setValidatorCompiler, setSerializerCompiler} = fastify().withTypeProvider<ZodTypeProvider>();
+
+register(fastifyCookie)
+register(fastifyJwt, { secret: 'supersecret-omniF' })
+
+register(fastifyCors, {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+});
+
+register(fastifySwagger, {
+    swagger: {
+        consumes: ["application/json"],
+        produces: ["application/json"],
+        info: {
+            title: "My.Tree API",
+            description: "Rotas do My.Tree",
+            version: "1.0.0",
+        },
+    },
+    transform: jsonSchemaTransform,
+})
+
+register(fastifySwaggerUi, {
+    routePrefix: "/docs",
+})
+
+register(multer.contentParser);
+
+setValidatorCompiler(validatorCompiler);
+setSerializerCompiler(serializerCompiler);
+register(auth)
+
+
+listen({
+    port: 3100,
+    host: "0.0.0.0",
+}, () => console.log('Server is running on port 3100'));
