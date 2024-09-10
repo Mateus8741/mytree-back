e33dce73a3a1a27f41787ba2d37841554946a79c
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-09-10 10:14:43 -0300

    Update eslint configuration to ignore additional directories and enable TypeScript linting

--- src/routes/Auth/registerUser.ts
+++ src/routes/Auth/registerUser.ts
@@ -1,19 +1,19 @@
-import { FastifyInstance } from "fastify";
-import { ZodTypeProvider } from "fastify-type-provider-zod";
-import { RegisterSchema } from "../../schemas/registerSchema";
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { RegisterSchema } from '../../schemas/registerSchema'
 
 export async function registerUser(app: FastifyInstance) {
-    app.withTypeProvider<ZodTypeProvider>().post(
-        "/register",
-        {
-            schema: {
-                body: RegisterSchema,
-            },
-        },
-        async (request, reply) => {
-            const { email, password } = request.body;
-            console.log(email, password);
-            reply.send({ email, password });
-        }
-    )
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/register',
+    {
+      schema: {
+        body: RegisterSchema,
+      },
+    },
+    async (request, reply) => {
+      const { email, password } = request.body
+      console.log(email, password)
+      reply.send({ email, password })
+    }
+  )
 }
