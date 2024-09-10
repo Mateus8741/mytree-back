374675c9c747260cde5c6945cd5f007093248e40
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-09-10 14:06:33 -0300

    Refactor package.json to include db:seed script for seeding the database

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,3 +1,41 @@
+import bcrypt from "bcryptjs";
+import { prisma } from "./prisma-client";
+
 async function seed() {
-    
+    const users = [
+        {
+            email: 't@t.com',
+            password: '12345678',
+        },
+        {
+            email: 'teste@t.com',
+            password: '12345678',
+        },
+        {
+            email: 'test@t.com',
+            password: '12345678',
+        }
+    ];
+
+    for (const user of users) {
+        const hashedPassword = await bcrypt.hash(user.password, 10);
+
+        await prisma.user.create({
+            data: {
+                email: user.email,
+                password: hashedPassword,
+            },
+        });
+    }
+
+    console.log("Users seeded successfully!");
 }
+
+seed()
+    .catch((e) => {
+        console.error(e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
